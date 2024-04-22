4b18c2c2f8845fb51c0b3c7cb7c9e70a1d893183
jianghao <jianghao@haplox.com>
2024-04-22 13:48:35 +0800

    fix: 更新天气的key值

--- src/views/dashboard/weather.js
+++ src/views/dashboard/weather.js
@@ -2,10 +2,11 @@ import axios from "axios";
 import { ref } from "vue";
 
 const weatherInfo = ref("今日晴，0℃ - 10℃，天气寒冷，注意添加衣物。");
-const amapKey = "0e72b5ebe0f0faa7daf26936fe6e5218";
+const amapKey = "20b275679ce09dc305e93ea8c0aaf1f3";
 
 export const useWeatherInfo = () => {
   ip();
+  // getWeather();
   return weatherInfo;
 };
 
@@ -21,7 +22,7 @@ export const ip = async () => {
 };
 
 const getWeather = async (code) => {
-  const response = await axios.get("https://restapi.amap.com/v3/weather/weatherInfo?key=" + amapKey + "&extensions=base&city=" + code);
+  const response = await axios.get("https://restapi.amap.com/v3/weather/weatherInfo?key=" + amapKey + "&extensions=all&city=" + code);
   if (response.data.info === "OK") {
     const s = response.data.lives[0];
     weatherInfo.value = s.city + " 天气：" + s.weather + " 温度：" + s.temperature + "℃ 风向：" + s.winddirection + " 风力：" + s.windpower + "级 空气湿度：" + s.humidity + "%";

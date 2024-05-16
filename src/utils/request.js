d6916fff18c8da9f9c34119484faec469a5b82b4
jianghao <jianghao@haplox.com>
2024-05-16 14:49:49 +0800

    fix: merge

--- src/utils/request.js
+++ src/utils/request.js
@@ -172,6 +172,26 @@ service.interceptors.response.use(
             cancelButtonText: "取消",
           },
         );
+      case 502:
+        ElMessageBox.confirm(
+          `
+        <p>检测到接口错误${error}</p>
+        <p>错误码<span style="color:red"> 500 </span>：此类错误内容常见于后台panic，请先查看后台日志，如果影响您正常使用可强制登出清理缓存</p>
+        `,
+          "接口报错",
+          {
+            dangerouslyUseHTMLString: true,
+            distinguishCancelAndClose: true,
+            confirmButtonText: "清理缓存",
+            cancelButtonText: "取消",
+          },
+        ).then(() => {
+          const userStore = useUserStore();
+          userStore.token = "";
+          localStorage.clear();
+          router.push({ name: "Login", replace: true });
+        });
+        break;
         break;
     }
 

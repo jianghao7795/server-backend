262c4d74a2261fdf58614f2b5cceaf5eb2b82620
jianghao <jianghao@haplox.com>
2024-11-18 14:36:37 +0800

    feat: 更新delpy.sh

--- deloy.sh
+++ deloy.sh
@@ -2,6 +2,7 @@
 
 ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|grep -v 172.\*.\*.\*|awk '{print $2}'|tr -d "addr:"`
 echo $ip
+hostip="192.168.56.103"
 
 # sed -i `s/192.168.56.103/$ip/g` './conf.d/my.conf'
 
@@ -11,5 +12,26 @@ docker stop backend
 docker rm backend
 docker rmi backend
 
-docker build --progress=plain -t backend .
+if [ $ip == $hostip ]
+then
+  docker build --progress=plain -t backend .
+else
+  docker build --progress=plain -f DockerfileHome -t backend .
+fi
+
 docker run --name backend -d -p 9000:9000 backend
+
+# a=10
+# b=20
+# if [ $a == $b ]
+# then
+#    echo "a 等于 b"
+# elif [ $a -gt $b ]
+# then
+#    echo "a 大于 b"
+# elif [ $a -lt $b ]
+# then
+#    echo "a 小于 b"
+# else
+#    echo "没有符合的条件"
+# fi

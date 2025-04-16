24b0f61a82223664871e5d44cb755edd79c28e90
jianghao <jianghao@haplox.com>
2025-04-16 15:24:29 +0800

    fix: 修复点击我时 黑框的出现

--- src/views/about/draggable.vue
+++ src/views/about/draggable.vue
@@ -1,49 +1,49 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import draggable from "vuedraggable";
+  import { ref, onMounted } from "vue";
+  import draggable from "vuedraggable";
 
-const drag = ref(false);
+  const drag = ref(false);
 
-const gridLists = ref([
-  { grid: "cn", num: 1 },
-  { grid: "cn", num: 2 },
-  { grid: "cn", num: 3 },
-  { grid: "cn", num: 4 },
-  { grid: "cn", num: 5 },
-  { grid: "cn", num: 6 },
-  { grid: "cn", num: 7 },
-  { grid: "cn", num: 8 },
-  { grid: "cn", num: 9 },
-]);
+  const gridLists = ref([
+    { grid: "cn", num: 1 },
+    { grid: "cn", num: 2 },
+    { grid: "cn", num: 3 },
+    { grid: "cn", num: 4 },
+    { grid: "cn", num: 5 },
+    { grid: "cn", num: 6 },
+    { grid: "cn", num: 7 },
+    { grid: "cn", num: 8 },
+    { grid: "cn", num: 9 },
+  ]);
 
-const lists = ref([
-  { people: "cn", id: 1, name: "www.itxst.com" },
-  { people: "cn", id: 2, name: "www.baidu.com" },
-  { people: "cn", id: 3, name: "www.taobao.com" },
-  { people: "cn", id: 4, name: "www.google.com" },
-]);
+  const lists = ref([
+    { people: "cn", id: 1, name: "www.itxst.com" },
+    { people: "cn", id: 2, name: "www.baidu.com" },
+    { people: "cn", id: 3, name: "www.taobao.com" },
+    { people: "cn", id: 4, name: "www.google.com" },
+  ]);
 
-const cutLists = ref([
-  { people: "cn", id: 1, name: "cut1" },
-  { people: "cn", id: 2, name: "cut2" },
-  { people: "cn", id: 3, name: "cut3" },
-  { people: "cn", id: 4, name: "cut4" },
-]);
+  const cutLists = ref([
+    { people: "cn", id: 1, name: "cut1" },
+    { people: "cn", id: 2, name: "cut2" },
+    { people: "cn", id: 3, name: "cut3" },
+    { people: "cn", id: 4, name: "cut4" },
+  ]);
 
-const change = (evt) => {
-  console.log("evt: ", evt);
-};
+  const change = (evt) => {
+    console.log("evt: ", evt);
+  };
 
-onMounted(() => {
-  // 使用原生sortable实现元素位置切换
-  new Sortable(document.querySelector(".cut-container"), {
-    swap: true,
-    forceFallback: true,
-    chosenClass: "chosen",
-    swapClass: "highlight",
-    animation: 300,
+  onMounted(() => {
+    // 使用原生sortable实现元素位置切换
+    new Sortable(document.querySelector(".cut-container"), {
+      swap: true,
+      forceFallback: true,
+      chosenClass: "chosen",
+      swapClass: "highlight",
+      animation: 300,
+    });
   });
-});
 </script>
 
 <template>
@@ -58,7 +58,7 @@ onMounted(() => {
                 <span>grid列表拖拽</span>
               </div>
             </template>
-            <draggable v-model="gridLists" class="grid-container" item-key="grid" animation="300" chosenClass="chosen" forceFallback="true">
+            <draggable v-model="gridLists" class="grid-container" item-key="grid" :animation="300">
               <template #item="{ element }">
                 <div :class="'item' + ' ' + 'item-' + element.num">
                   {{ element.num }}
@@ -75,35 +75,35 @@ onMounted(() => {
               </div>
             </template>
             <!-- 单列拖拽 -->
-            <draggable v-model="lists" item-key="name" @change="change" chosen-class="chosen" force-fallback="true" animation="300">
+            <draggable v-model="lists" item-key="name" @change="change" :animation="300">
               <template #item="{ element }">
                 <div class="item-single">{{ element.name }}</div>
               </template>
             </draggable>
           </el-card>
         </el-col>
-        <el-col :xs="25" :sm="8" :md="8" :lg="8">
+        <!-- <el-col :xs="25" :sm="8" :md="8" :lg="8">
           <el-card>
             <template #header>
               <div class="card-header">
                 <span>拖拽实现元素位置切换</span>
               </div>
             </template>
-            <!-- 拖拽实现元素位置切换 -->
+            <!-- 拖拽实现元素位置切换 
             <!-- <draggable v-model="cutLists" @start="drag = true" @end="drag = false" item-key="name" :animation="300">
               <template #item="{ element }">
                 <div class="item-cut">
                   <p>{{ element.name }}</p>
                 </div>
               </template>
-            </draggable> -->
+            </draggable>
             <div class="cut-container">
               <div class="item-cut" v-for="(item, index) in cutLists" :key="index">
                 <p>{{ item.name }}</p>
               </div>
             </div>
           </el-card>
-        </el-col>
+        </el-col> -->
         <el-col :xs="25" :sm="8" :md="8" :lg="8">
           <el-card>
             <template #header>
@@ -127,80 +127,81 @@ onMounted(() => {
 </template>
 
 <style lang="scss" scoped>
-/* grid列表拖拽 */
-.grid-container {
-  display: grid;
-  grid-template-columns: 33.3% 33.3% 33.3%;
-  grid-template-rows: 33.3% 33.3% 33.3%;
-}
 
-.item-single {
-  font-size: 1.5em;
-  height: 77px;
-  text-align: center;
-  line-height: 85px;
-  border: 1px solid #e5e4e9;
-  cursor: move;
-}
-
-.item-cut {
-  font-size: 1.5em;
-  height: 77px;
-  line-height: 77px;
-  text-align: center;
-  border: 1px solid #e5e4e9;
-  cursor: move;
-}
-
-.item {
-  font-size: 2em;
-  text-align: center;
-  line-height: 100px;
-  border: 1px solid #e5e4e9;
-  cursor: move;
-
-  @media screen and (max-width: 750px) {
-    line-height: 90px;
+  /* grid列表拖拽 */
+  .grid-container {
+    display: grid;
+    grid-template-columns: 33.3% 33.3% 33.3%;
+    grid-template-rows: 33.3% 33.3% 33.3%;
   }
-}
 
-.item-1 {
-  background-color: #ef342a;
-}
+  .item-single {
+    font-size: 1.5em;
+    height: 77px;
+    text-align: center;
+    line-height: 85px;
+    border: 1px solid #e5e4e9;
+    cursor: move;
+  }
 
-.item-2 {
-  background-color: #f68f26;
-}
+  .item-cut {
+    font-size: 1.5em;
+    height: 77px;
+    line-height: 77px;
+    text-align: center;
+    border: 1px solid #e5e4e9;
+    cursor: move;
+  }
 
-.item-3 {
-  background-color: #4ba946;
-}
+  .item {
+    font-size: 2em;
+    text-align: center;
+    line-height: 100px;
+    border: 1px solid #e5e4e9;
+    cursor: move;
 
-.item-4 {
-  background-color: #0376c2;
-}
+    @media screen and (max-width: 750px) {
+      line-height: 90px;
+    }
+  }
 
-.item-5 {
-  background-color: #c077af;
-}
+  .item-1 {
+    background-color: #ef342a;
+  }
 
-.item-6 {
-  background-color: #f8d29d;
-}
+  .item-2 {
+    background-color: #f68f26;
+  }
 
-.item-7 {
-  background-color: #b5a87f;
-}
+  .item-3 {
+    background-color: #4ba946;
+  }
 
-.item-8 {
-  background-color: #d0e4a9;
-}
+  .item-4 {
+    background-color: #0376c2;
+  }
 
-.item-9 {
-  background-color: #4dc7ec;
-}
+  .item-5 {
+    background-color: #c077af;
+  }
 
-.chosen {
-  border: solid 2px #3089dc !important;
-}
+  .item-6 {
+    background-color: #f8d29d;
+  }
+
+  .item-7 {
+    background-color: #b5a87f;
+  }
+
+  .item-8 {
+    background-color: #d0e4a9;
+  }
+
+  .item-9 {
+    background-color: #4dc7ec;
+  }
+
+  .chosen {
+    border: solid 2px #3089dc !important;
+  }
 </style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -11,7 +11,8 @@
         <el-backtop :right="100" :bottom="100"></el-backtop>
       </el-aside>
       <!-- 分块滑动功能 -->
-      <el-main class="main-cont main-right" :style="{ width: `calc(100% - ${isMobile ? '0px' : isCollapse ? '50px' : '220px'})` }">
+      <el-main class="main-cont main-right"
+        :style="{ width: `calc(100% - ${isMobile ? '0px' : isCollapse ? '50px' : '220px'})` }">
         <transition :duration="{ enter: 800, leave: 100 }" mode="out-in" name="el-fade-in-linear">
           <div class="topfix">
             <el-row>
@@ -44,9 +45,12 @@
                       <div class="right-box">
                         <el-dropdown trigger="click">
                           <span class="el-dropdown-link">
-                            <translate theme="filled" size="20" fill="#333" strokeLinejoin="miter" strokeLinecap="square" />
+                            <translate theme="filled" size="20" fill="#333" strokeLinejoin="miter"
+                              strokeLinecap="square" />
                             {{ languageSelect[language] }}
-                            <el-icon><CaretBottom /></el-icon>
+                            <el-icon>
+                              <CaretBottom />
+                            </el-icon>
                           </span>
                           <template #dropdown>
                             <el-dropdown-menu @click="toggle">
@@ -56,8 +60,8 @@
                           </template>
                         </el-dropdown>
                         <Search />
-                        <el-dropdown @visible-change="changeMouse">
-                          <div class="dp-flex justify-content-center align-items height-full width-full">
+                        <el-dropdown @visible-change="changeMouse" trigger="click">
+                          <div>
                             <span class="header-avatar" style="cursor: pointer">
                               <CustomPic />
                               <span style="margin-left: 5px">
@@ -71,10 +75,16 @@
                           <template #dropdown>
                             <el-dropdown-menu class="dropdown-group">
                               <el-dropdown-item>
-                                <span style="font-weight: 600">当前角色：{{ userStore.userInfo.authority?.authorityName }}</span>
+                                <span style="font-weight: 600">当前角色：
+                                  {{
+                                    userStore.userInfo.authority?.authorityName
+                                  }}
+                                </span>
                               </el-dropdown-item>
                               <template v-if="userStore.userInfo.authorities">
-                                <el-dropdown-item v-for="item in userStore.userInfo.authorities.filter((i) => i.authorityId !== userStore.userInfo.authorityId)" :key="item.authorityId" @click="changeUserAuth(item.authorityId)">
+                                <el-dropdown-item
+                                  v-for="item in userStore.userInfo.authorities.filter((i) => i.authorityId !== userStore.userInfo.authorityId)"
+                                  :key="item.authorityId" @click="changeUserAuth(item.authorityId)">
                                   <span>切换为：{{ item.authorityName }}</span>
                                 </el-dropdown-item>
                               </template>
@@ -95,7 +105,8 @@
             <HistoryComponent ref="layoutHistoryComponent" />
           </div>
         </transition>
-        <router-view v-if="reloadFlag" v-slot="{ Component, route }" v-loading="loadingFlag" element-loading-text="正在加载中" class="admin-box">
+        <router-view v-if="reloadFlag" v-slot="{ Component, route }" v-loading="loadingFlag"
+          element-loading-text="正在加载中" class="admin-box">
           <div v-bind:id="'refreshView'">
             <!-- el-fade-in-linear -->
             <transition mode="out-in" name="el-fade-in-linear" type="transition" :appear="true">
@@ -113,214 +124,214 @@
 </template>
 
 <script>
-export default {
-  name: "Layout",
-};
+  export default {
+    name: "Layout",
+  };
 </script>
 
 <script setup>
-import Aside from "@/views/layout/aside/index.vue";
-import HistoryComponent from "@/views/layout/aside/historyComponent/history.vue";
-import Search from "@/views/layout/search/search.vue";
-import BottomInfo from "@/views/layout/bottomInfo/bottomInfo.vue";
-import CustomPic from "@/components/customPic/index.vue";
-import Setting from "./setting/index.vue";
-import { setUserAuthority } from "@/api/user";
-import { emitter } from "@/utils/bus.js";
-import { computed, ref, onMounted, nextTick, inject } from "vue";
-import { useRouter, useRoute } from "vue-router";
-import { useUserStore } from "@/pinia/modules/user";
-import { useAuthorityStore } from "@/pinia/modules/authority";
-import { useRouterStore } from "@/pinia/modules/router";
-import { fmtTitle } from "@/utils/fmtRouterTitle";
-import { Translate } from "@icon-park/vue-next";
-// import { useRefreshStore } from "@/pinia/modules/refresh";
+  import Aside from "@/views/layout/aside/index.vue";
+  import HistoryComponent from "@/views/layout/aside/historyComponent/history.vue";
+  import Search from "@/views/layout/search/search.vue";
+  import BottomInfo from "@/views/layout/bottomInfo/bottomInfo.vue";
+  import CustomPic from "@/components/customPic/index.vue";
+  import Setting from "./setting/index.vue";
+  import { setUserAuthority } from "@/api/user";
+  import { emitter } from "@/utils/bus.js";
+  import { computed, ref, onMounted, nextTick, inject } from "vue";
+  import { useRouter, useRoute } from "vue-router";
+  import { useUserStore } from "@/pinia/modules/user";
+  import { useAuthorityStore } from "@/pinia/modules/authority";
+  import { useRouterStore } from "@/pinia/modules/router";
+  import { fmtTitle } from "@/utils/fmtRouterTitle";
+  import { Translate } from "@icon-park/vue-next";
+  // import { useRefreshStore } from "@/pinia/modules/refresh";
 
-// const refreshStore = useRefreshStore();
+  // const refreshStore = useRefreshStore();
 
-// watchEffect(() => {
-//   console.log(refreshStore.isRefresh);
-// });
+  // watchEffect(() => {
+  //   console.log(refreshStore.isRefresh);
+  // });
 
-// const locale = inject("locale");
-const toggle = inject("toggle");
-const language = inject("language");
-// console.log(locale, language);
-const languageSelect = {
-  en: "English",
-  "zh-CN": "简体中文",
-};
-
-const router = useRouter();
-const route = useRoute();
-const routerStore = useRouterStore();
-const authorityStore = useAuthorityStore();
-// 三种窗口适配
-const isCollapse = ref(false);
-const isSider = ref(true);
-const isMobile = ref(false);
-const initPage = () => {
-  const screenWidth = document.body.clientWidth;
-  if (screenWidth < 1000) {
-    isMobile.value = true;
-    isSider.value = false;
-    isCollapse.value = true;
-  } else if (screenWidth >= 1000 && screenWidth < 1200) {
-    isMobile.value = false;
-    isSider.value = false;
-    isCollapse.value = true;
-  } else {
-    isMobile.value = false;
-    isSider.value = true;
-    isCollapse.value = false;
-  }
-};
-
-initPage();
-
-const loadingFlag = ref(false);
-onMounted(() => {
-  // 挂载一些通用的事件
-  emitter.emit("collapse", isCollapse.value); // emit 是值 fire an event
-  emitter.emit("mobile", isMobile.value);
-  emitter.on("reload", reload); // on 倾听所有事件 // emitter.all.clear() clearing all events
-  emitter.on("showLoading", () => {
-    loadingFlag.value = true;
-  });
-  emitter.on("closeLoading", () => {
-    loadingFlag.value = false;
-  });
-  window.onresize = () => {
-    // onresize 是调整浏览器尺寸时触发
-    return (() => {
-      initPage();
-      emitter.emit("collapse", isCollapse.value);
-      emitter.emit("mobile", isMobile.value);
-    })();
+  // const locale = inject("locale");
+  const toggle = inject("toggle");
+  const language = inject("language");
+  // console.log(locale, language);
+  const languageSelect = {
+    en: "English",
+    "zh-CN": "简体中文",
   };
-  if (userStore.loadingInstance) {
-    userStore.loadingInstance.close();
-  }
 
-  authorityStore.getAuthority();
-});
-const userStore = useUserStore();
-const textColor = computed(() => {
-  if (userStore.sideMode === "dark") {
-    return "#fff";
-  } else if (userStore.sideMode === "light") {
-    return "#191a23";
-  } else {
-    return userStore.baseColor;
-  }
-});
+  const router = useRouter();
+  const route = useRoute();
+  const routerStore = useRouterStore();
+  const authorityStore = useAuthorityStore();
+  // 三种窗口适配
+  const isCollapse = ref(false);
+  const isSider = ref(true);
+  const isMobile = ref(false);
+  const initPage = () => {
+    const screenWidth = document.body.clientWidth;
+    if (screenWidth < 1000) {
+      isMobile.value = true;
+      isSider.value = false;
+      isCollapse.value = true;
+    } else if (screenWidth >= 1000 && screenWidth < 1200) {
+      isMobile.value = false;
+      isSider.value = false;
+      isCollapse.value = true;
+    } else {
+      isMobile.value = false;
+      isSider.value = true;
+      isCollapse.value = false;
+    }
+  };
 
-const backgroundColor = computed(() => {
-  if (userStore.sideMode === "dark") {
-    return "#191a23";
-  } else if (userStore.sideMode === "light") {
-    return "#fff";
-  } else {
-    return userStore.sideMode;
-  }
-});
+  initPage();
 
-const matched = computed(() => route.meta.matched);
+  const loadingFlag = ref(false);
+  onMounted(() => {
+    // 挂载一些通用的事件
+    emitter.emit("collapse", isCollapse.value); // emit 是值 fire an event
+    emitter.emit("mobile", isMobile.value);
+    emitter.on("reload", reload); // on 倾听所有事件 // emitter.all.clear() clearing all events
+    emitter.on("showLoading", () => {
+      loadingFlag.value = true;
+    });
+    emitter.on("closeLoading", () => {
+      loadingFlag.value = false;
+    });
+    window.onresize = () => {
+      // onresize 是调整浏览器尺寸时触发
+      return (() => {
+        initPage();
+        emitter.emit("collapse", isCollapse.value);
+        emitter.emit("mobile", isMobile.value);
+      })();
+    };
+    if (userStore.loadingInstance) {
+      userStore.loadingInstance.close();
+    }
 
-const changeUserAuth = async (id) => {
-  const res = await setUserAuthority({
-    authorityId: id,
+    authorityStore.getAuthority();
   });
-  if (res.code === 200) {
-    emitter.emit("closeAllPage");
-    setTimeout(() => {
-      window.location.reload();
-    }, 1);
-  }
-};
+  const userStore = useUserStore();
+  const textColor = computed(() => {
+    if (userStore.sideMode === "dark") {
+      return "#fff";
+    } else if (userStore.sideMode === "light") {
+      return "#191a23";
+    } else {
+      return userStore.baseColor;
+    }
+  });
 
-const reloadFlag = ref(true);
+  const backgroundColor = computed(() => {
+    if (userStore.sideMode === "dark") {
+      return "#191a23";
+    } else if (userStore.sideMode === "light") {
+      return "#fff";
+    } else {
+      return userStore.sideMode;
+    }
+  });
 
-// watchEffect((newValue, oldValue) => {
-//   console.log(reloadFlag.value);
-// });
-let reloadTimer = null;
-const reload = async () => {
-  if (reloadTimer) {
-    window.clearTimeout(reloadTimer);
-  }
-  // reloadTimer = window.setTimeout(async () => {
-  //   if (route.meta.keepAlive) {
-  //     reloadFlag.value = false;
-  //     await nextTick();
-  //     reloadFlag.value = true;
-  //   } else {
-  //     const title = route.meta.title;
-  //     router.push({ name: "Reload", params: { title } });
-  //   }
-  // }, 400);
-  if (route.meta.keepAlive) {
-    reloadFlag.value = false;
-    await nextTick();
-    reloadFlag.value = true;
-  } else {
-    const title = route.meta.title;
-    router.push({ name: "Reload", params: { title } });
-  }
-};
+  const matched = computed(() => route.meta.matched);
 
-const isShadowBg = ref(false);
-const totalCollapse = () => {
-  isCollapse.value = !isCollapse.value;
-  isSider.value = !isCollapse.value;
-  isShadowBg.value = !isCollapse.value;
-  emitter.emit("collapse", isCollapse.value);
-};
+  const changeUserAuth = async (id) => {
+    const res = await setUserAuthority({
+      authorityId: id,
+    });
+    if (res.code === 200) {
+      emitter.emit("closeAllPage");
+      setTimeout(() => {
+        window.location.reload();
+      }, 1);
+    }
+  };
 
-const toPerson = () => {
-  router.push({ name: "person" });
-};
-const changeShadow = () => {
-  isShadowBg.value = !isShadowBg.value;
-  isSider.value = !!isCollapse.value;
-  totalCollapse();
-};
+  const reloadFlag = ref(true);
 
-const returnHome = () => {
-  router.push({ name: "dashboard" });
-};
+  // watchEffect((newValue, oldValue) => {
+  //   console.log(reloadFlag.value);
+  // });
+  let reloadTimer = null;
+  const reload = async () => {
+    if (reloadTimer) {
+      window.clearTimeout(reloadTimer);
+    }
+    // reloadTimer = window.setTimeout(async () => {
+    //   if (route.meta.keepAlive) {
+    //     reloadFlag.value = false;
+    //     await nextTick();
+    //     reloadFlag.value = true;
+    //   } else {
+    //     const title = route.meta.title;
+    //     router.push({ name: "Reload", params: { title } });
+    //   }
+    // }, 400);
+    if (route.meta.keepAlive) {
+      reloadFlag.value = false;
+      await nextTick();
+      reloadFlag.value = true;
+    } else {
+      const title = route.meta.title;
+      router.push({ name: "Reload", params: { title } });
+    }
+  };
 
-const mouseLeaveOrEnter = ref(false);
+  const isShadowBg = ref(false);
+  const totalCollapse = () => {
+    isCollapse.value = !isCollapse.value;
+    isSider.value = !isCollapse.value;
+    isShadowBg.value = !isCollapse.value;
+    emitter.emit("collapse", isCollapse.value);
+  };
 
-const changeMouse = (e) => {
-  mouseLeaveOrEnter.value = e;
-};
+  const toPerson = () => {
+    router.push({ name: "person" });
+  };
+  const changeShadow = () => {
+    isShadowBg.value = !isShadowBg.value;
+    isSider.value = !!isCollapse.value;
+    totalCollapse();
+  };
+
+  const returnHome = () => {
+    router.push({ name: "dashboard" });
+  };
+
+  const mouseLeaveOrEnter = ref(false);
+
+  const changeMouse = (e) => {
+    mouseLeaveOrEnter.value = e;
+  };
 </script>
 
 <style lang="scss">
-@import "@/style/mobile.scss";
+  @import "@/style/mobile.scss";
 
-.dark {
-  background-color: #191a23 !important;
-  color: #fff !important;
-}
+  .dark {
+    background-color: #191a23 !important;
+    color: #fff !important;
+  }
 
-.light {
-  background-color: #fff !important;
-  color: #000 !important;
-}
+  .light {
+    background-color: #fff !important;
+    color: #000 !important;
+  }
 
-.mouse-enter-leave {
-  transform: rotate(180deg);
-}
+  .mouse-enter-leave {
+    transform: rotate(180deg);
+  }
 
-.switch-mouse {
-  transition: transform 0.5s;
-}
+  .switch-mouse {
+    transition: transform 0.5s;
+  }
 
-.el-dropdown-link {
-  margin-right: 15px;
-  display: flex;
-  align-items: center;
-}
+  .el-dropdown-link {
+    margin-right: 15px;
+    display: flex;
+    align-items: center;
+  }
 </style>
